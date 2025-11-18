25905f1ec7cccc45110b61bfd5de7d2a6e7db7c3
a-arham-x <108538325+a-arham-x@users.noreply.github.com>
2025-11-18 22:26:49 +0500

    developed why recce page

---
+++ src/components/whyRecce/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import ShowSelection from "@/public/svg/ShowSelection";
+
+function Card({ title, description, imgUrl }: { title: string, description: string, imgUrl: string }) {
+	return (
+		<div className="p-4 rounded-[20px] bg-gradient-to-r from-[#ffffff1a] to-[#282828] flex flex-col">
+			<div className="w-[392px] h-[231px] rounded-[14px] overflow-hidden">
+				<img src={imgUrl} className="w-full h-full object-cover" alt="icon" />
+			</div>
+			<p className='mt-4 mb-2 text-2xl'>{title}</p>
+			<p className='text-white/60'>{description}</p>
+		</div>
+	)
+}
+
+export default Card
